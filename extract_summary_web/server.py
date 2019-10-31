653841818eb4a4892cc077c8c952421f8fb4a26b
lics1216 <670159244@qq.com>
2019-10-31 10:58:27 +0800

    【开发】项目代码

---
+++ extract_summary_web/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, send_from_directory, send_file, jsonify
+from utils import  db
+import json
+app = Flask(__name__, static_url_path='')
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return send_file('static/index.html')
+
+@app.route('/static/<path:path>', methods=['GET'])
+def send_static(path):
+    return send_from_directory('static', path)
+
+@app.route('/comment/<int:begin_id>/<int:offset>', methods=['GET'])
+def get_comment(begin_id, offset):
+    fields = ['game_id','game_name','summaries']
+    ja = db.get_comment(fields, begin_id, offset)
+    for c in ja:
+        c['summaries'] = json.loads(c['summaries'])
+    return jsonify(ja)
+
+@app.route('/last_comment/<int:end_id>/<int:offset>', methods=['GET'])
+def last_comment(end_id, offset):
+    fields = ['game_id','game_name','summaries']
+    ja = db.last_comment(fields, end_id, offset)
+    for c in ja:
+        c['summaries'] = json.loads(c['summaries'])
+    return jsonify(ja)    
+
+# @app.route('/comment', methods=['POST'])
+# def update_comment():
+#     j = request.get_json()
+#     if 'emotion' in j:
+#         field = 'emotion'
+#         value = j['emotion']
+#     elif 'type' in j:
+#         field = 'type'
+#         value = j['type']
+#     else:
+#         return jsonify({'res':'error', 'paras':j})
+#     if db.update_comment(j['id'], field, value):
+#         return jsonify({'res': 'ok'})   
+#     else:
+#         return jsonify({'res':'error', 'paras':j})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8003)
---
+++ extract_summary_web/utils/db.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-06-12 17:35:58
+# @Author  : yangchaojun (YYChildren@gmail.com)
+# @Link    : https://git.mingchao.com/yangchaojun
+# @Version : 1.0.0
+
+import pymysql
+
+
+def get_comment(fields, index1, index2):
+    fs = ",".join(fields)             
+    sql = "SELECT %s FROM c_lcs_game_comment_summary where cast(game_id as signed)>=%s order by cast(game_id as signed) asc limit %s" % (fs, index1, index2)
+    conn = pymysql.connect(host='127.0.0.1',port=3306,user='root',password='eXYhzAWjyvy8grwM',db='game_process',charset='utf8mb4')
+    # 创建mysql 游标
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    # fetchall 获取all数据、 fetchone 获取单条
+    # a = [1, 2, 3] b = [4, 5, 6] zip(a, b) = [(1,4), (2,5), (3,6)]
+    rs = [dict(zip(fields, i)) for i in cursor.fetchall()]
+    cursor.close()
+    conn.close()
+    return rs
+
+def last_comment(fields, index1, index2):
+    fs = ",".join(fields)             
+    sql = "select * from (SELECT %s FROM c_lcs_game_comment_summary where cast(game_id as signed)<=%s order by cast(game_id as signed) desc limit %s) a  order by cast(a.game_id as signed) asc" % (fs, index1, index2)
+    conn = pymysql.connect(host='127.0.0.1',port=3306,user='root',password='eXYhzAWjyvy8grwM',db='game_process',charset='utf8mb4')
+    # 创建mysql 游标
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    # fetchall 获取all数据、 fetchone 获取单条
+    # a = [1, 2, 3] b = [4, 5, 6] zip(a, b) = [(1,4), (2,5), (3,6)]
+    rs = [dict(zip(fields, i)) for i in cursor.fetchall()]
+    cursor.close()
+    conn.close()
+    return rs
+
+# def update_comment(index, field, value):
+#     sql = "UPDATE s_lcs_game_comments_qq SET %s=%s WHERE id=%s" % (field, value, index)
+#     conn = pymysql.connect(host='127.0.0.1',port=3306,user='root',password='eXYhzAWjyvy8grwM',db='game_process',charset='utf8mb4',autocommit=True)
+#     cursor = conn.cursor()
+#     cursor.execute(sql)
+#     cursor.close()
+#     conn.close()
+#     return True
